e29d325dfc9f1db098dc31dc018fd5a8344bae25
Shizz74 <wojciechbajko@gmail.com>
2025-11-10 13:09:54 +0100

    TBAJO-009 zmiana ml na kilogramy

--- routes/products.js
+++ routes/products.js
@@ -1,72 +1,72 @@
-const router = require('express').Router();
-const productSchema = require('../models/product');
-
+const router = require("express").Router();
+const productSchema = require("../models/product");
 
 //Create a new product
-router.post('/product', async (req, res) => {
+router.post("/product", async (req, res) => {
+  const productExist = await productSchema.findOne({ name: req.body.name });
+  if (productExist) return res.status(400).send("product_exist");
 
-    const productExist = await productSchema.findOne({ name: req.body.name });
-    if (productExist) return res.status(400).send('product_exist');
+  console.log(req);
+  let amount;
 
-    console.log(req);
-    let amount;
+  if (req.body.unit === "sztuk" || req.body.unit === "kilogramów") {
+    amount = 1;
+  } else if (req.body.unit === "gram") {
+    amount = 100;
+  }
 
-    if(req.body.unit === 'sztuk') {
-        amount = 1;
-    } else if (req.body.unit === 'gram') {
-        amount = 100;
-    } else if (req.body.unit === 'ml') {
-        amount = 1000;
-    }
+  //Create product
+  const product = new productSchema({
+    name: req.body.name,
+    categoryId: req.body.categoryId,
+    unit: req.body.unit,
+    amount: amount,
+    amountToBuy: 0,
+  });
 
-
-    //Create product
-    const product = new productSchema({
-        name: req.body.name,
-        categoryId: req.body.categoryId,
-        unit: req.body.unit,
-        amount: amount,
-        amountToBuy: 0
-    });
-
-    try {
-        const savedProduct = await product.save();
-        res.send(savedProduct);
-    } catch (err) {
-        res.status(400).send(err);
-    }
-})
+  try {
+    const savedProduct = await product.save();
+    res.send(savedProduct);
+  } catch (err) {
+    res.status(400).send(err);
+  }
+});
 
 //Get all products and connect them with category from other collection
-router.get('/products', async (req, res) => {
-    //"Find" with empty parametr (object) return all record
-    productSchema.find({})
+router.get("/products", async (req, res) => {
+  //"Find" with empty parametr (object) return all record
+  productSchema
+    .find({})
     .populate("categoryId")
-    .then(function(products){
-        res.send(products);
-    })
-})
+    .then(function (products) {
+      res.send(products);
+    });
+});
 
 //Get specific product
-router.get('/product/:id', async (req, res) => {
-    productSchema.findById({_id: req.params.id}).then(function(product){
-        res.send(product);
-    })
-})
+router.get("/product/:id", async (req, res) => {
+  productSchema.findById({ _id: req.params.id }).then(function (product) {
+    res.send(product);
+  });
+});
 
 //Delete specific product
-router.delete('/product/:id', async (req, res) => {
-    productSchema.findByIdAndDelete({_id: req.params.id}).then(function(product){
-        res.send(product);
-    })
-    //Add error when e.g element doesn't exist
-})
+router.delete("/product/:id", async (req, res) => {
+  productSchema
+    .findByIdAndDelete({ _id: req.params.id })
+    .then(function (product) {
+      res.send(product);
+    });
+  //Add error when e.g element doesn't exist
+});
 
 //Edit specific product
 
-router.put('/product/update/:id',  async (req, res) => {
-    productSchema.findByIdAndUpdate({_id: req.params.id}, req.body).then(function(req){
-        res.send(req);
-    })
-})
+router.put("/product/update/:id", async (req, res) => {
+  productSchema
+    .findByIdAndUpdate({ _id: req.params.id }, req.body)
+    .then(function (req) {
+      res.send(req);
+    });
+});
 module.exports = router;
